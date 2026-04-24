c9f6331b3d9121f9c8f23e4c780566de52878524
Igor Agapov <aio350@yahoo.com>
2026-04-24 14:38:08 +0500

    update locale name

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const locales = ['ru-RU', 'en-US', 'nl-NL', 'fr-FR', 'ja-JP', 'ar-EG']
 export const localeNames = [
   'Русский',
   'Английский (США)',
-  'Датский',
+  'Нидерландский (голландский)',
   'Французский',
   'Японский',
   'Арабский',
